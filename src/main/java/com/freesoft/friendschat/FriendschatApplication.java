0599d4360307ca01404daab35c254544887a219a
SagidKamilov <89615854074a@gmail.com>
2024-08-15 20:50:12 +0300

    Структура проекта

---
+++ src/main/java/com/freesoft/friendschat/FriendschatApplication.java
@@ -0,0 +1,13 @@
+package com.freesoft.friendschat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendschatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendschatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freesoft/friendschat/FriendschatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freesoft.friendschat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendschatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
